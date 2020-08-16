8064ff2185c182b5bfdff23828671831f879689d
SrikanthMudapaka <srikanthsr.m9@gmail.com>
2020-08-16 18:05:27 +0530

    HazelCast_CRUD_Operations

---
+++ Spring_Boot_HazelCast_example/src/main/java/com/example/cache/api/model/User.java
@@ -0,0 +1,56 @@
+package com.example.cache.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity(name = "User_table")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -6912201477750422475L;
+	@Id
+	private Integer id;
+	private String name;
+	private String address;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(Integer id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	
+}
